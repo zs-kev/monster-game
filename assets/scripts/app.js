599ca819f01621a992988c5e736afadbe0a7f92c
Kevin Simon <keviniansimon@gmail.com>
2020-10-12 16:45:51 +0200

    Refactor some conditionals to switch

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -32,14 +32,19 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
             finalMonsterHealth: monsterHealth,
             finalPlayerHealth: playerHealth
     };
-    if(event === LOG_EVENT_PLAYER_ATTACK) {
-        logEntry.target = 'MONSTER'
-    } else if(event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-        logEntry.target = 'MONSTER'
-    } else if(event === LOG_EVENT_MONSTER_ATTACK) {
-        logEntry.target = 'PLAYER'
-    } else if(event === LOG_EVENT_PLAYER_HEAL) {
-        logEntry.target = 'PLAYER'
+    switch (event) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry.target = 'PLAYER';
+            break;
     }
     battleLog.push(logEntry);
 }
